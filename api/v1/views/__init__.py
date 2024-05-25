16ec61468211fc6e65638485bf7829eea89f40ec
Lamba237 <simontanyi2004@gmail.com>
2024-05-25 16:31:19 -0700

    updates

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,6 +2,9 @@
 """ import flask packages"""
 
 from flask import Blueprint
-from . import views
+
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+if app_views is not None:
+    from api.v1.views.index import *
