67c14acff17866305542d7a67867f6853ed2ea77
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2024-05-26 18:21:21 +0100

    Error Handling ++

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,6 +6,7 @@ using flask to start a web application
 from api.v1.views import app_views
 from flask import Flask, jsonify
 from models import storage
+from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
